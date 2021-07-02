f9c1e896953fbf46d78475102daed74dcbe48a74
RichBarbosa <70115573+RichBarbosa@users.noreply.github.com>
2021-07-01 23:52:24 -0300

    Initial commit

---
+++ CadastroDev20/app/src/main/java/com/richard/cadastrodev20/teste.kt
@@ -0,0 +1,4 @@
+package com.richard.cadastrodev20
+
+class teste {
+}
---
+++ cadastrodev 2.0/app/src/main/java/com/richard/cadastrodev/Usuario.kt
@@ -0,0 +1,13 @@
+package com.richard.cadastrodev
+
+import android.graphics.drawable.Drawable
+
+data class Usuario (
+    var foto: Drawable? =null,
+    var nome: String,
+    var email: String,
+    var stack: Stack,
+    var senioridade: Senioridade,
+    var empregado: Boolean
+
+)
